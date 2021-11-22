e8fe6787b997d77ce0e5e0b3e828c0415eb9f533
avantonder <ajv37@cam.ac.uk>
2021-11-22 17:34:56 +0000

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,12 +2,12 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'pathgenevocam'
+copyright = '2021'
+author = 'ajv37'
 
-release = '0.1'
-version = '0.1.0'
+release = ''
+version = ''
 
 # -- General configuration
 
